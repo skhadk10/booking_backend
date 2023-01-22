fef4f3f829b6155507cf5e3a8dc436ee4b524eb9
skhadk10 <rickeykhd@gmail.com>
2023-01-22 16:48:40 +1100

    stripe connection is done

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,36 +1,37 @@
-import User from "../models/user"
-import Stripe from "stripe"
-const queryString = require('querystring');
+import User from "../models/user";
+import Stripe from "stripe";
+const queryString = require("querystring");
 
-const stripe= new Stripe(process.env.STRIPE_SECRET)
-export const createConnectAccount=async(req, res, ) =>{
-console.log("sign in is required",req.auth);
-// find user from db
-const user =await User.findById(req.auth._id).exec()
-console.log("user====>",user)
-// if user dont have stripe_account_id yet, create new
-if(!user.stripe_account_id){
-    const account =await stripe.accounts.create({
-        type:'express',
-    })
-    console.log("Account===>",account);
-    user.stripe_account_id=account.id
-    user.save()
-}
-// create account link based on account id(for frontend to complete onboarding)
-let accountLink = await stripe.accountLinks.create({
+const stripe = new Stripe(process.env.STRIPE_SECRET);
+export const createConnectAccount = async (req, res) => {
+  console.log("sign in is required", req.auth);
+  // find user from db
+  const user = await User.findById(req.auth._id).exec();
+  console.log("user====>", user);
+  // if user dont have stripe_account_id yet, create new
+  if (!user.stripe_account_id) {
+    const account = await stripe.accounts.create({
+      type: "express",
+    });
+    console.log("Account===>", account);
+    user.stripe_account_id = account.id;
+    user.save();
+  }
+  // // create account link based on account id(for frontend to complete onboarding)
+  let accountLink = await stripe.accountLinks.create({
     account: user.stripe_account_id,
     refresh_url: process.env.STRIPE_REDIRECT_URL,
     return_url: process.env.STRIPE_REDIRECT_URL,
     type: "account_onboarding",
   });
-  // prefill any info such as email
+  //   // prefill any info such as email
   accountLink = Object.assign(accountLink, {
     "stripe_user[email]": user.email || undefined,
   });
-  let link=`${accountLink.url}?${queryString(accountLink)}`
-  res.send(link)
-// update payment schedule(olptional.default is 2 days)
+  console.log(accountLink, "account linked");
 
-
-}
+  let link = `${accountLink.url}?${queryString.stringify(accountLink)}`;
+  console.log(link, "linked");
+  res.send(link);
+  // update payment schedule(olptional.default is 2 days)
+};
